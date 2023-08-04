69905829ec39fc9e1a07d6f47ed6145ce496af9b
Stuart Littlefair <s.littlefair@shef.ac.uk>
2023-08-04 21:16:47 +0100

    remove scripts

--- python/setup.py
+++ python/setup.py
@@ -7,46 +7,50 @@ import os, sys
 import platform
 
 library_dirs = []
-include_dirs = ['./include']
+include_dirs = ["./include"]
 
 # need to direct to where includes and  libraries are
-if 'TRM_SOFTWARE' in os.environ:
-    library_dirs.append(os.path.join(os.environ['TRM_SOFTWARE'], 'lib'))
-    include_dirs.append(os.path.join(os.environ['TRM_SOFTWARE'], 'include'))
+if "TRM_SOFTWARE" in os.environ:
+    library_dirs.append(os.path.join(os.environ["TRM_SOFTWARE"], "lib"))
+    include_dirs.append(os.path.join(os.environ["TRM_SOFTWARE"], "include"))
 else:
-    print ("Environment variable TRM_SOFTWARE pointing to location of shareable libraries and includes not defined!",file=sys.stderr)
+    print(
+        "Environment variable TRM_SOFTWARE pointing to location of shareable libraries and includes not defined!",
+        file=sys.stderr,
+    )
     sys.exit(-1)
 include_dirs.append(numpy.get_include())
 
-if platform.system() == 'Darwin':
+if platform.system() == "Darwin":
     ext_modules = [
-        Extension("lfit",
+        Extension(
+            "lfit",
             ["lfit.pyx"],
-            include_dirs = include_dirs,
-            library_dirs = library_dirs,
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
             extra_compile_args=["-stdlib=libc++"],
             extra_link_args=["-stdlib=libc++"],
             language="c++",
-            libraries = ["subs","roche"]
+            libraries=["subs", "roche"],
         )
     ]
 else:
     ext_modules = [
-        Extension("lfit",
+        Extension(
+            "lfit",
             ["lfit.pyx"],
-            include_dirs = include_dirs,
-            library_dirs = library_dirs,
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
             language="c++",
-            libraries = ["subs","roche"]
+            libraries=["subs", "roche"],
         )
     ]
 
 setup(
-    name = "lfit",
+    name="lfit",
     version="0.15",
     description="Calculate and fit CV lightcurves",
-    ext_modules = cythonize(ext_modules),
-    url = "https://github.com/StuartLittlefair/lfit",
-    author_email = "s.littlefair@shef.ac.uk",
-    scripts = ['scripts/calcPhysicalParams.py']
+    ext_modules=cythonize(ext_modules),
+    url="https://github.com/StuartLittlefair/lfit",
+    author_email="s.littlefair@shef.ac.uk",
 )
