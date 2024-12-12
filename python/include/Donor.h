a508c4b6b5e7c19029346b4616043411903ce9ba
Stuart Littlefair <s.littlefair@shef.ac.uk>
2024-12-12 12:58:52 +0000

    fixed bug in donor

--- python/include/Donor.h
+++ python/include/Donor.h
@@ -6,44 +6,52 @@
 #include "trm/roche.h"
 #include "Point.h"
 
-namespace LFIT{
+namespace LFIT
+{
 
-    class Donor{
-        
+    class Donor
+    {
+
     public:
         //! Default constructor
-        Donor() : tiles(), beta(0.08), gmin(), ulimb(0.9),
-        q(1.0e30), ntiles(400), normalisation(-1.){}
+        Donor() : tiles(), beta(0.08), gmin(), ulimb(0.5),
+                  q(1.0e30), ntiles(400), normalisation(-1.) {}
         //! Constructor
-        Donor(const double& q_, const size_t& size) : tiles(),beta(0.08),
-        gmin(),ulimb(0.9),q(1.0e30),ntiles(size),normalisation(-1.){
+        Donor(const double &q_, const size_t &size) : tiles(), beta(0.08),
+                                                      gmin(), ulimb(0.5), q(1.0e30), ntiles(size), normalisation(-1.)
+        {
             this->tweak(q_);
         }
-        void tweak(const double& q);
-        double calcFlux(const double& phi, const double& incl);
-        double calcFlux(const double& phi, const double& width,
-                        const double& incl);
-        void setup_grid(const double& incl);
+        void tweak(const double &q);
+        double calcFlux(const double &phi, const double &incl);
+        double calcFlux(const double &phi, const double &width,
+                        const double &incl);
+        void setup_grid(const double &incl);
         Subs::Buffer1D<double> getBright();
-        double get_q(){
+        double get_q()
+        {
             return this->q;
         }
-        void set_q(double q){
+        void set_q(double q)
+        {
             this->tweak(q);
         }
-        int get_size(){
+        int get_size()
+        {
             return this->tiles.size();
         }
-        void set_size(int size){
+        void set_size(int size)
+        {
             this->tiles.resize(size);
             this->ntiles = size;
             this->tweak(this->q);
         }
+
     private:
         Subs::Buffer1D<LFIT::Point> tiles;
-        double beta; 
-        double gmin; 
-        double ulimb; 
+        double beta;
+        double gmin;
+        double ulimb;
         double q;
         int ntiles;
         double normalisation;
@@ -51,28 +59,27 @@ namespace LFIT{
 
     //! Function object to compute Roche potential and its derivative
     /**
-     * This is needed for 'rtsafe' 
+     * This is needed for 'rtsafe'
      */
-    class Lfunc : public Subs::RTfunc {
-        
+    class Lfunc : public Subs::RTfunc
+    {
+
         double qp, cpot;
         Subs::Vec3 x0, x1;
-        
+
     public:
-        
         //! Constructor storing fixed data
-        Lfunc(Subs::Vec3 x0i, Subs::Vec3 x1i, double qpi, double cpoti) : 
-        qp(qpi), cpot(cpoti), x0(x0i), x1(x1i) {}
-        
+        Lfunc(Subs::Vec3 x0i, Subs::Vec3 x1i, double qpi, double cpoti) : qp(qpi), cpot(cpoti), x0(x0i), x1(x1i) {}
+
         //! Function operator
-        void operator()(double t, double& f, double& d) const { 
+        void operator()(double t, double &f, double &d) const
+        {
             Subs::Vec3 p, dp;
-            p = x0 + t*x1;
-            f   = Roche::rpot(qp, p)-cpot;
-            dp  = Roche::drpot(qp, p);
-            d   = Subs::dot(dp,x1);
+            p = x0 + t * x1;
+            f = Roche::rpot(qp, p) - cpot;
+            dp = Roche::drpot(qp, p);
+            d = Subs::dot(dp, x1);
         }
-        
     };
-    
+
 }
--- python/src/Donor.cc
+++ python/src/Donor.cc
@@ -8,10 +8,15 @@
 #include "trm/constants.h"
 #include "geometry.h"
 
-void LFIT::Donor::tweak(const double& q_){
-    
-    if(q_ == this->q){
-        //No need to recalculate donor star
+#include <iostream>
+#include <fstream>
+
+void LFIT::Donor::tweak(const double &q_)
+{
+
+    if (q_ == this->q)
+    {
+        // No need to recalculate donor star
         return;
     }
     this->q = q_;
@@ -21,162 +26,160 @@ void LFIT::Donor::tweak(const double& q_){
     this->normalisation = -1.;
 }
 
-void LFIT::Donor::setup_grid(const double& incl){
-    
+void LFIT::Donor::setup_grid(const double &incl)
+{
+
     double q = this->q;
-    
-    // find inner lagrangian pt (distance from 1ry in units of separation)
-    double xl1 = Roche::xl1(q);
-    // set vector a to XL1 point (xl1,0,0)
-    Subs::Vec3 a = Subs::Vec3(xl1,0.0,0.0);
-    // calculate critical potential at xl1 point
-    double cpot = Roche::rpot(q,a);
-    
+
+    // Calculate a reference radius and potential
+    double rref2, pref2;
+    Roche::ref_sphere(q, Roche::SECONDARY, 1.0, 1.0, rref2, pref2);
+
+    // variables for finding roche surface
+    // position of surface, normal to surface and direction for search
+    Subs::Vec3 posn, normvec, dirn;
+    double rad, gravity;
+    double ACC = 1.0e-6;
+
     // now find the back of 2ry
-    a.set(1.0,0.0,0.0);
-    Subs::Vec3 b(a);
-    // set reasonable limits for length along vec b
-    double t1 = 0.05*(1.0-xl1);
-    double t2 = 1.10*(1.0-xl1);
-    // solve to find critical potential along b....
-    double t = Subs::rtsafe(Lfunc(a,b,q,cpot),t1,t2,1.0e-6);
-    double xback = 1.0+t; // x-coord of back of donor
-    
-    //also find pole
-    b.set(0.0,0.0,1.0);
-    t2 = sqrt(Subs::sqr(1.01*(1.0-xl1))-Subs::sqr(a.x()-1.0));
-    t1 = t2/1.0e2;
-    // solve to find critical potential along b....
-    t = Subs::rtsafe(Lfunc(a,b,q,cpot),t1,t2,1.0e-6);
-    Subs::Vec3 pole(a+t*b);
-    // calculate derivative of roche potential at pole
-    Subs::Vec3 grad = Roche::drpot(q,pole);
-    this->gmin = grad.length();
-    
+    dirn.set(1., 0., 0.);
+    Roche::face(q, Roche::SECONDARY, 1.0, dirn, rref2, pref2, ACC, posn, normvec, rad, gravity);
+    // Compute reference gravity value, from the side of the star opposite from the L1 point
+    this->gmin = gravity;
+
     // OK, let's make the roche surface
-    int icount=-1;
+    int icount = -1;
     int np, nmer;
-    Subs::Vec3 xHat = Subs::Vec3(1.0,0.0,0.0);
+    Subs::Vec3 xHat = Subs::Vec3(1.0, 0.0, 0.0);
+    Subs::Vec3 yHat = Subs::Vec3(0.0, 1.0, 0.0);
     np = int(sqrt(this->ntiles));
     this->tiles.resize(this->ntiles);
 
     // create tiles
     LFIT::Point::etype eclipses;
-    nmer=np;
-    double delta_x = (xback-xl1)*double(nmer-1)/(double(nmer))/double(nmer+1);
-    for(int i=0; i< nmer; ++i){
-        a.set(xl1 + (xback-xl1)*double(i+1)/double(nmer+1),0.0,0.0);
-        for(int j=0; j<np; ++j){
+    nmer = np;
+    double dphi = Constants::TWOPI / double(np);
+    double dtheta = Constants::PI / double(nmer);
+    std::ofstream outfile;
+    outfile.open("faces.txt");
+
+    for (int i = 0; i < nmer; ++i)
+    {
+        // start at theta = 0, pointing to back of donor
+        double theta = Constants::PI * (i + 0.5) / double(nmer);
+        double sint = sin(theta);
+        double cost = cos(theta);
+
+        for (int j = 0; j < np; ++j)
+        {
             icount++;
             // b points to surface of star at various angles
-            double theta = Constants::TWOPI*double(j)/double(np);
-            b.set(0.0,cos(theta),sin(theta));
-            // limit search along b to sensible values
-            t2 = sqrt(Subs::sqr(1.01*(1.0-xl1))-Subs::sqr(a.x()-1.0));
-            t1 = t2/1.0e2;
-            // solve to find critical potential along b....
-            t = Subs::rtsafe(Lfunc(a,b,q,cpot),t1,t2,1.0e-6);
-            
-            // so this is surface of star
-            Subs::Vec3 posn = a + t*b;
-            // normal to tile is direction of derivative
-            Subs::Vec3 dirn = Roche::drpot(q,posn);
-            double gravity = dirn.length();
-            dirn.unit();
-            
-            
+            double phi = Constants::TWOPI * double(j) / double(np);
+            double sinp = sin(phi);
+            double cosp = cos(phi);
+
+            dirn.set(cost, sint * cosp, sint * sinp);
+            Roche::face(q, Roche::SECONDARY, 1.0, dirn, rref2, pref2, ACC, posn, normvec, rad, gravity);
+
             // ingress, egress phases
-            //eclipses.clear();
-            //double ingress, egress;
-            //if (Roche::ingress_egress(q, Roche::SECONDARY, 1.0, 1.0, incl, 1.0e-5, posn, ingress, egress)){
+            // eclipses.clear();
+            // double ingress, egress;
+            // if (Roche::ingress_egress(q, Roche::SECONDARY, 1.0, 1.0, incl, 1.0e-5, posn, ingress, egress)){
             //    eclipses.push_back(std::make_pair(ingress,egress));
-            //}            
-                        
+            //}
+
             // we also need the element area, which is the circumference
             // of the roche lobe at this point, divided by the number
             // of theta steps, and multiplied by delta_x/cos(alpha),
             // where alpha is angle between element and x-axis
             // cirumference is twopi*t
             // area needs to be multiplied by sep**2.0 to become physical
-            double cos_alpha = cos( asin(Subs::dot(dirn,xHat) ) );
-            double area = delta_x*Constants::TWOPI*t/double(np)/cos_alpha;
+            double area = rad * rad * sint * dphi * dtheta;
+            area /= Subs::dot(dirn, normvec);
 
             // now set temperature of element, scaling for limb and gravity darkening
             // temp should be multiplied by pow(grav/gmin,beta)
-            double temp = 3000.0*pow(gravity/this->gmin,this->beta);
+            double temp = 3000.0 * pow(gravity / this->gmin, this->beta);
 
             // flux, not accounting for limb darkening
-            double flux = Subs::planck(6560.0,temp);
-
+            double flux = Subs::planck(6560.0, temp);
+            outfile << posn << " " << area << " " << gravity << " " << flux << " " << normvec << std::endl;
             // create point
-            this->tiles[icount] = LFIT::Point(posn,dirn,area,gravity,eclipses);
+            this->tiles[icount] = LFIT::Point(posn, normvec, area, gravity, eclipses);
             this->tiles[icount].flux = flux;
         }
     }
+    outfile.close();
 }
 
-
-double LFIT::Donor::calcFlux(const double& phi, const double& width,
-                             const double& incl){
+double LFIT::Donor::calcFlux(const double &phi, const double &width,
+                             const double &incl)
+{
     /*
     integrates over bin of finite phase width, width using trapezoidal int
     */
-    
-    double phi1 = phi - width/2.0;
-    double rflux=0.0;
+
+    double phi1 = phi - width / 2.0;
+    double rflux = 0.0;
     int nphi = 5;
-	
-    for(int i=0; i<5; i++){
-        double p = phi1 + width*double(i)/double(nphi-1);
-        if(i == 0 || i == nphi-1){
-            rflux += LFIT::Donor::calcFlux(p,incl)/2.0;
-        }else{
-            rflux += LFIT::Donor::calcFlux(p,incl);
+
+    for (int i = 0; i < 5; i++)
+    {
+        double p = phi1 + width * double(i) / double(nphi - 1);
+        if (i == 0 || i == nphi - 1)
+        {
+            rflux += LFIT::Donor::calcFlux(p, incl) / 2.0;
+        }
+        else
+        {
+            rflux += LFIT::Donor::calcFlux(p, incl);
         }
     }
-    rflux /= double(nphi-1);
-    
+    rflux /= double(nphi - 1);
+
     return rflux;
 }
 
-double LFIT::Donor::calcFlux(const double& phi, const double& incl){
-    
+double LFIT::Donor::calcFlux(const double &phi, const double &incl)
+{
     // have we been called without the tiles calculated
-    if (this->tiles.size() == 0){
+    if (this->tiles.size() == 0)
+    {
         std::cout << "LFIT::Calcflux shouldn't be called before calculating grid.\nThis is inefficient" << std::endl;
-        this->setup_grid(incl);   
+        this->setup_grid(incl);
     }
 
-	if(this->normalisation < 0.0){
-	    // maximum flux is at phi=0.75
-		double maxphi = 0.75;
-		Subs::Vec3 earth = Roche::set_earth(incl,maxphi);
-		double sum=0.0;
-	    //#pragma omp parallel for reduction(+:sum)
-		for(int i=0; i< this->tiles.size();i++){
-            double mu = Subs::dot(earth,this->tiles[i].dirn);
-            if(mu > -0.01 && this->tiles[i].visible(maxphi)){
-                double flux = this->tiles[i].flux * (1.-this->ulimb+fabs(mu)*this->ulimb);
-                sum+=flux*this->tiles[i].area*mu;
+    if (this->normalisation < 0.0)
+    {
+        // maximum flux is at phi=0.75
+        double maxphi = 0.75;
+        Subs::Vec3 earth = Roche::set_earth(incl, maxphi);
+        double sum = 0.0;
+        // #pragma omp parallel for reduction(+:sum)
+        for (int i = 0; i < this->tiles.size(); i++)
+        {
+            double mu = Subs::dot(earth, this->tiles[i].dirn);
+            if (mu > 0.0 && this->tiles[i].visible(maxphi))
+            {
+                double flux = this->tiles[i].flux * (1. - this->ulimb + fabs(mu) * this->ulimb);
+                sum += flux * this->tiles[i].area * mu;
             }
-		}
-		this->normalisation = sum;
-	}
-	
-    double sum=0.0;
-    Subs::Vec3 earth = Roche::set_earth(incl,phi);
-    //#pragma omp parallel for reduction(+:sum)
-    for(int i=0; i< this->tiles.size();i++){
-        double mu = Subs::dot(earth,this->tiles[i].dirn);
-        if(mu > -0.01 && this->tiles[i].visible(phi)){
-            double flux = this->tiles[i].flux * (1.-this->ulimb+fabs(mu)*this->ulimb);
-            sum+=flux*this->tiles[i].area*mu;
+        }
+        this->normalisation = sum;
+    }
+
+    double sum = 0.0;
+    Subs::Vec3 earth = Roche::set_earth(incl, phi);
+    // #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < this->tiles.size(); i++)
+    {
+        double mu = Subs::dot(earth, this->tiles[i].dirn);
+        if (mu > 0.0 && this->tiles[i].visible(phi))
+        {
+            double flux = this->tiles[i].flux * (1. - this->ulimb + fabs(mu) * this->ulimb);
+            sum += flux * this->tiles[i].area * mu;
         }
     }
 
-    return sum/this->normalisation;
+    return sum / this->normalisation;
 }
-
-   
-   
-    
